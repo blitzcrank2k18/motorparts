e14da7668c3ccda7e3cd68696a6c99d306f8d83d
Honeylee Magbanua <emoblazz@gmail.com>
2019-09-01 08:53:15 +0800

    chart

--- admin/chart.php
+++ admin/chart.php
@@ -1,20 +1,20 @@
 <?php
 include('../dist/includes/dbcon.php');
-	//require('db_config.php');
+  //require('db_config.php');
 
 
-	/* Getting demo_viewer table data */
-	//$sql = "select SUM(amount_due) from sales natural join sales_details natural join product group by MONTH(date_added)+'-'+YEAR(date_added)";
-//	$sql = "SELECT SUM(numberofview) as count FROM demo_viewer 
-//			GROUP BY YEAR(created_at) ORDER BY created_at";
-//	$viewer = mysqli_query($mysqli,$sql);
-//	$viewer = mysqli_fetch_all($viewer,MYSQLI_ASSOC);
-//	$viewer = json_encode(array_column($viewer, 'count'),JSON_NUMERIC_CHECK);
+  /* Getting demo_viewer table data */
+  //$sql = "select SUM(amount_due) from sales natural join sales_details natural join product group by MONTH(date_added)+'-'+YEAR(date_added)";
+//  $sql = "SELECT SUM(numberofview) as count FROM demo_viewer 
+//      GROUP BY YEAR(created_at) ORDER BY created_at";
+//  $viewer = mysqli_query($mysqli,$sql);
+//  $viewer = mysqli_fetch_all($viewer,MYSQLI_ASSOC);
+//  $viewer = json_encode(array_column($viewer, 'count'),JSON_NUMERIC_CHECK);
 
 
-	/* Getting demo_click table data */
-	$year=$_POST['year'];
-	$sql = "SELECT SUM(amount_due) as total,DATE_FORMAT(date_added,'%Y/%m') as date FROM sales where year(date_added)='$year' group by date";
+  /* Getting demo_click table data */
+  $year=$_POST['year'];
+  $sql = "SELECT SUM(total) as total,DATE_FORMAT(date_added,'%Y/%m') as date FROM `sales` where year(date_added)='$year' group by date";
                   $click = mysqli_query($con,$sql);
                   $click = mysqli_fetch_all($click,MYSQLI_ASSOC);
                   $date = json_encode(array_column($click, 'date'),JSON_NUMERIC_CHECK);
@@ -28,11 +28,11 @@ include('../dist/includes/dbcon.php');
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Yearly Sales Report</title>
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-	<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.js"></script>
-	<script src="../dist/js/highcharts.js"></script>
-	<style type="text/css">
+  <title>Yearly Sales Report</title>
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.js"></script>
+  <script src="../dist/js/highcharts.js"></script>
+  <style type="text/css">
       @media print {
           .btn-print {
             display:none !important;
@@ -81,9 +81,9 @@ $(function () {
 
 
 <div class="container">
-	<br/>
-	<h2 class="text-center">Yearly Sales Report for <?php echo $year;?>
-	 <input class="btn-print btn-primary" type="button" name="print" value="Print" onclick="window.print();window.location.href='overall.php';">            </h2>
+  <br/>
+  <h2 class="text-center">Yearly Sales Report for <?php echo $year;?>
+   <input class="btn-print btn-warning" type="button" name="print" value="Print" onclick="window.print();window.location.href='home.php';">            </h2>
     <div class="row">
         <div class="col-md-10 col-md-offset-1">
             <div class="panel panel-default">
@@ -97,7 +97,7 @@ $(function () {
                     </tr>
 <?php
     
-    $query=mysqli_query($con,"SELECT SUM(amount_due) as total,DATE_FORMAT(date_added,'%Y/%m') as date FROM sales where DATE_FORMAT(date_added,'%Y')='$year' GROUP BY date ORDER BY date_added")or die(mysqli_error($con));
+    $query=mysqli_query($con,"SELECT SUM(total) as total,DATE_FORMAT(date_added,'%Y/%m') as date FROM `sales` where year(date_added)='$year' group by date")or die(mysqli_error($con));
 
     while($row=mysqli_fetch_array($query)){
     
